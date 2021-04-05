fbe6f0524e0ecaf3c55de25349c87fb3b858a11f
ShannonCh <shannon26.c@gmail.com>
2021-04-05 15:28:09 -0400

    using JWT

---
+++ README.md
@@ -0,0 +1,4 @@
+README.md
+
+-. You should update your package.json "start:dev" script to say something like "start:dev": "JWT=somekeyhere nodemon server" You should call this variable from your code by saying process.env.JWT
+
---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { models: { User }} = require('./db');
+const path = require('path');
+const jwt = require('jsonwebtoken')
+
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+const SECRET_KEY = process.env.JWT
+
+app.post('/api/auth', async(req, res, next)=> {
+  try {
+    res.send({ token: await User.authenticate(req.body)});
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.get('/api/auth', async(req, res, next)=> {
+  try {
+
+    const user = await User.byToken(req.headers.authorization)
+    console.log(user)
+    res.send(user);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next)=> {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
+});
+
+module.exports = app;
